7ccaae7e941306c516157514fdca7ccd4c3b172e
irfanandriansyah1997 <irfanandriansyah10@gmail.com>
2017-08-19 00:02:24 +0700

    update code and bug fix in year filmography field

--- imdbCrawler/spiders/actress_detail.py
+++ imdbCrawler/spiders/actress_detail.py
@@ -11,9 +11,9 @@ class ActressDetailSpider(scrapy.Spider):
     name = "actress-detail"
     allowed_domains = []
     base_url = None
-    start_urls = ["http://www.imdb.com/name/nm3592338/"]
+    start_urls = []
     collection = None
-    pipeline = set([])
+    pipeline = set([MongoPipeline, RequiredFieldsPipeline])
     required_fields = ["actress_name", "actress_category", "actress_filmography", "actress_height"]
     mongo_requirement = {
         "primary": "actress_id",
@@ -26,8 +26,13 @@ class ActressDetailSpider(scrapy.Spider):
         self.base_url = data.get("base_url")
         self.collection = data.get("collection").get("actress")
         self.allowed_domains = [x for x in data.get("allowed_domains")]
+        self.db = MongoPipeline(
+                    data.get("database").get("host"),
+                    data.get("database").get("port"),
+                    data.get("database").get("db")
+                )
 
-        print self.start_urls
+        self.start_urls = self.populate_start_urls()
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -35,7 +40,24 @@ class ActressDetailSpider(scrapy.Spider):
         data.update({"base_url": crawler.settings.get("BASE_URL")})
         data.update({"collection": crawler.settings.get("COLLECTION")})
         data.update({"allowed_domains": crawler.settings.get("ALLOWED_DOMAIN")})
+        data.update({"database" : {
+            "host" : crawler.settings.get("MONGODB_HOST"),
+            "port" : crawler.settings.get("MONGODB_PORT"),
+            "db" : crawler.settings.get("MONGODB_DB")
+        }})
         return cls(data)
+
+    def populate_start_urls(self):
+        BASE_URL = "http://www.imdb.com/name/"
+        db = self.db.get(
+                "actress",
+                where={
+                    "actress_category" : {"$exists" : False},
+                    "actress_filmography" : {"$exists" : False}
+                }
+        )
+
+        return ['{}{}'.format(BASE_URL, a.get('actress_id')) for a in db.get('data')]
 
     def parse(self, response):
         name = response.css("#overview-top > h1 > span.itemprop::text").extract_first().strip()
@@ -66,11 +88,14 @@ class ActressDetailSpider(scrapy.Spider):
         return there.sub('', text)
 
     def parsingFilm(self, component):
-        tempYear = component.css("span.year_column::text").extract_first().strip().split('-')
+        tempYear = filter(None, component.css("span.year_column::text").extract_first().strip().split('-'))
 
-        year = {'start': int(tempYear[0]), 'end': int(tempYear[1])} \
-            if len(tempYear) > 1 \
-            else {'start': int(tempYear[0]), 'end': int(tempYear[0])}
+        if len(tempYear) > 0:
+            year = {'start': int(re.sub('[^0-9.]+', '', tempYear[0])), 'end': int(re.sub('[^0-9.]+', '', tempYear[1]))} \
+                if len(tempYear) > 1 \
+                else {'start': int(re.sub('[^0-9.]+', '', tempYear[0])), 'end': int(re.sub('[^0-9.]+', '', tempYear[0]))}
+        else:
+            year = {'start': 'Info Not Found', 'end': 'Info Not Found'}
 
         film = component.css("b > a::text").extract_first().strip()
 
